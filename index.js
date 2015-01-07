4323d52753638f629e84dcfab1a194774e920dc5
luin <i@zihua.li>
2015-01-07 12:46:33 +0800

    Add default path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function isDir(obj) {
 function handle(app, prefix, routes) {
   Object.keys(routes).forEach(function(key) {
     if (isDir(routes[key])) {
-      handle(app, prefix + '/' + key, routes[key]);
+      handle(app, prefix[prefix.length - 1] === '/' ? prefix + key : prefix + '/' + key, routes[key]);
     } else {
       app.use(module.exports.mount(prefix, routes[key].middleware()));
     }
@@ -13,6 +13,10 @@ function handle(app, prefix, routes) {
 }
 
 module.exports = function(app, prefix, path) {
+  if (arguments.length === 2) {
+    path = prefix;
+    prefix = '';
+  }
   var routes = require('./lib/require-dir')(path);
   handle(app, prefix, routes);
 };
